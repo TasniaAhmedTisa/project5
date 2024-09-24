aa85540dc35d47d5592d157e856daf0c919580de
TasniaAhmedTisa <tasniaahmed055@gmail.com>
2024-09-25 00:25:11 +0600

    add history part into donation page

--- button.js
+++ button.js
@@ -1,9 +1,23 @@
+//show section by id
+function showSectionById(id){
+    document.getElementById('donation-container').classList.add('hidden')
+    document.getElementById('history-container').classList.add('hidden')
+
+    document.getElementById(id).classList.remove('hidden')
+
+}
 document.getElementById('blogButton').onclick = function () {
     window.location.href = 'blog.html'; 
 };
-document.getElementById('donationBtn').onclick = function () {
-    window.location.href = 'donation.html'; 
-};
-document.getElementById('historyBtn').onclick = function () {
-    window.location.href = 'history.html'; 
-};
+
+document.getElementById('historyBtn').addEventListener('click', function(){
+    showSectionById('history-container')
+})
+
+
+document.getElementById('donationBtn').addEventListener('click', function(){
+    showSectionById('donation-container') 
+} );
+
+
+
